c4cfec9e9c3919d83682db62ec1e971db64f7886
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-27 20:37:52 +0100

    Adiciona componente de carrinho de compras

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,9 +1,11 @@
 <script setup> 
    import Heading from '@/components/template/web/cart/Heading.vue'
+   import Cart from '@/components/template/web/cart/Cart.vue'
 </script>
 
 <template>
     <div>
        <Heading />
+       <Cart />
     </div>
 </template>
