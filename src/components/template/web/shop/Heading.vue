d623aa0690de4a5256892db35a6e3151671ff1d0
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-27 17:28:04 +0100

    Adiciona componente de cabeçalho da loja

---
+++ src/components/template/web/shop/Heading.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <div class="cs_height_140 cs_height_lg_80"></div>
+    <section class="cs_page_heading text-center position-relative cs_bg_filed" data-src="assets/img/shop_bg.jpg">
+        <div class="cs_hero_overlay position-absolute"></div>
+        <div class="container position-relative">
+            <h2 class="cs_fs_50 cs_bold cs_white_color mb-0">Shop</h2>
+        </div>
+    </section>
+</template>
