8076e8df98cf40512d52c00b6ba183674dbf65d8
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-27 16:00:19 +0100

    Adiciona componente de categorias

---
+++ src/components/template/web/home/Category.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container-fluid">
+    <div class="cs_grid_list">
+      <div class="cs_category cs_style_1">
+        <a href="shop.html" class="cs_category_thumb position-relative">
+          <img src="@/assets/img/men.jpg" alt="Category Image" class="w-100">
+          <span class="cs_category_btn">
+            <span>Homens</span>
+            <span><i class="fa-solid fa-arrow-right"></i></span>
+          </span>
+        </a>
+      </div>
+      <div class="cs_category cs_style_1">
+        <a href="shop.html" class="cs_category_thumb position-relative">
+          <img src="@/assets/img/women.jpg" alt="Category Image" class="w-100">
+          <span class="cs_category_btn">
+            <span>Mulheres</span>
+            <span><i class="fa-solid fa-arrow-right"></i></span>
+          </span>
+        </a>
+      </div>
+      <div class="cs_category cs_style_1">
+        <a href="shop.html" class="cs_category_thumb position-relative">
+          <img src="@/assets/img/children.jpg" alt="Category Image" class="w-100">
+          <span class="cs_category_btn">
+            <span>Crianças</span>
+            <span><i class="fa-solid fa-arrow-right"></i></span>
+          </span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Hero from '@/components/template/web/home/Hero.vue'
 import IconBoxes from '@/components/template/web/home/IconBoxes.vue'
+import Category from '@/components/template/web/home/Category.vue'
 import { onMounted } from 'vue';
 import {main} from '@/assets/js/main.js'
 
@@ -12,4 +13,5 @@ onMounted(() => {
 <template>
   <Hero />
   <IconBoxes />
+  <Category />
 </template>
