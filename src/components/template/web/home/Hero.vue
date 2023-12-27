0f3852962701870e82b341888fc146a991c882b7
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-27 15:37:18 +0100

    Adiciona componente de Hero

--- src/components/template/web/home/Hero.vue
+++ src/components/template/web/home/Hero.vue
@@ -1,4 +1,11 @@
+<script setup>
+   import imagePath1 from "@/assets/img/hero1.jpg"
+   import imagePath2 from "@/assets/img/hero3.jpg"
+   import imagePath3 from "@/assets/img/hero4.jpg"
+
+</script>
 <template>
+   <div class="cs_height_140 cs_height_lg_80"></div>
    <section class="cs_slider position-relative">
     <div class="cs_slider_container" data-autoplay="0" data-loop="1" data-speed="800" data-center="0" data-slides-per-view="1" data-fade-slide="0">
       <div class="cs_slider_wrapper">
@@ -9,7 +16,7 @@
               <p class="cs_heto_subtitle">we are more than just an online shopping destination; we're your personal gateway to a world of fashion, trendsetting styles, and unparalleled convenience. We believe that fashion is a reflection of your individuality, and our mission is to make every shopping experience with us.</p>
               <a href="shop.html" class="cs_btn cs_style_1 cs_fs_16 cs_medium">Shop Now</a>
             </div>
-            <div class="cs_hero_thumb cs_bg_filed" data-src="assets/img/hero1.jpg"></div>
+            <div class="cs_hero_thumb cs_bg_filed" :style="{ 'background-image': `url(${imagePath1})` }"></div>
           </div>
         </div>
         <div class="slick_slide_in">
@@ -19,7 +26,7 @@
               <p class="cs_heto_subtitle">we are more than just an online shopping destination; we're your personal gateway to a world of fashion, trendsetting styles, and unparalleled convenience. We believe that fashion is a reflection of your individuality, and our mission is to make every shopping experience with us.</p>
               <a href="shop.html" class="cs_btn cs_style_1 cs_fs_16 cs_medium">Shop Now</a>
             </div>
-            <div class="cs_hero_thumb cs_bg_filed" data-src="assets/img/hero3.jpg"></div>
+            <div class="cs_hero_thumb cs_bg_filed" :style="{ 'background-image': `url(${imagePath2})` }"></div>
           </div>
         </div>
         <div class="slick_slide_in">
@@ -29,7 +36,7 @@
               <p class="cs_heto_subtitle">we are more than just an online shopping destination; we're your personal gateway to a world of fashion, trendsetting styles, and unparalleled convenience. We believe that fashion is a reflection of your individuality, and our mission is to make every shopping experience with us.</p>
               <a href="shop.html" class="cs_btn cs_style_1 cs_fs_16 cs_medium">Shop Now</a>
             </div>
-            <div class="cs_hero_thumb cs_bg_filed" data-src="assets/img/hero4.jpg">
+            <div class="cs_hero_thumb cs_bg_filed" :style="{ 'background-image': `url(${imagePath3})` }">
             </div>
           </div>
         </div>
