bf8d1fe5d3df786abe85a20da0a95b4743c78573
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-27 14:49:27 +0100

    Adiciona componente de cabeçalho

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { onMounted } from 'vue';
 import Header from '@/components/template/web/Nav/Header.vue'
+import {main} from '@/assets/js/main.js'
+
+onMounted(() => {
+  main();
+});
+
 </script>
 
 <template>
