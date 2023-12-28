f173b2fb8e3491b3e7cf4b6354978a94094c8b28
matondojoao <matondojoaokitemoco@gmail.com>
2023-12-28 14:41:43 +0100

    Instalando e configurando axios

---
+++ src/services/http.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+    baseURL: 'https://api.ecommerce.aviatocreative.com/v1',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default axiosInstance
