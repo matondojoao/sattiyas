d63f092afe2ee414d5fb3c02ff60e981b8fd2b95
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-06 14:51:45 +0100

    Adicionar remoção de item da lista de desejos e atualização automática

--- src/views/WishlistView.vue
+++ src/views/WishlistView.vue
@@ -3,12 +3,20 @@ import Heading from '@/components/template/web/wishlist/Heading.vue'
 import Wishlist from '@/components/template/web/wishlist/Wishlist.vue'
 import { onMounted } from 'vue';
 import { main } from '@/assets/js/main.js'
+import Perloader from '@/components/template/web/Perloader.vue'
+
+const showPerloader = ref(true);
+
 onMounted(async () => {
     main();
+    setTimeout(() => {
+       showPerloader.value = false;
+  }, 2000);
 });
 </script>
 
 <template>
+    <Perloader v-if="showPerloader" />
     <Heading />
     <Wishlist />
 </template>
