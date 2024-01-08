a759d4c02d7e55bd5f67e915dfdc7f4e11d9a2fd
matondojoao <matondojoaokitemoco@gmail.com>
2024-01-08 14:12:35 +0100

    Adicionar cálculo do total do carrinho

--- src/components/template/web/cart/Cart.vue
+++ src/components/template/web/cart/Cart.vue
@@ -1,94 +1,109 @@
 <script setup>
-  import { RouterLink } from "vue-router";
-  import { computed, onMounted } from "vue";
-  import { useShoppingCartStore  } from '@/stores/cart';
+import { RouterLink } from "vue-router";
+import { computed, onMounted } from "vue";
+import { useShoppingCartStore } from '@/stores/cart';
 
-  const shoppingCartStore = useShoppingCartStore();
-  
+const shoppingCartStore = useShoppingCartStore();
+
 onMounted(async () => {
 });
 
- function removeFromCart(product_id){
+function removeFromCart(product_id) {
   shoppingCartStore.removeFromCart(product_id)
 }
-  const items = computed(()=>{
-    return shoppingCartStore.getCartItems
-  });
+function incrementQuantity(product_id,quantity){
+  shoppingCartStore.updateQuantity(product_id,quantity + 1);
+};
+function decrementQuantity(product_id,quantity){
+  if (item.quantity > 0) {
+    shoppingCartStore.updateQuantity(product_id,quantity - 1);
+  }
+};
+const items = computed(() => {
+  return shoppingCartStore.getCartItems
+});
+
+const totalCart = computed(() => {
+  return shoppingCartStore.getTotalCart
+});
 
 </script>
 
 <template>
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-8">
-          <div class="table-responsive">
-            <table class="cs_cart_table">
-              <thead>
-                <tr>
-                  <th>Produto</th>
-                  <th>Preço</th>
-                  <th>Quantidade</th>
-                  <th>Subtotal</th>
-                  <th></th>
-                </tr>
-              </thead>
+  <div class="container">
+    <div class="row">
+      <div class="col-xl-8">
+        <div class="table-responsive">
+          <table class="cs_cart_table">
+            <thead>
+              <tr>
+                <th>Produto</th>
+                <th>Preço</th>
+                <th>Quantidade</th>
+                <th>Subtotal</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items" :key="item.product_id">
+                <td>
+                  <div class="cs_cart_table_media">
+                    <img :src="item.product_image" alt="Thumb">
+                    <h3>{{ item.product_name }}</h3>
+                  </div>
+                </td>
+                <td>{{ item.price }}</td>
+                <td>
+                  <div class="cs_quantity">
+                    <button class="cs_quantity_btn cs_increment" @click="incrementQuantity(item.product_id,item.quantity)"><i
+                        class="fa-solid fa-angle-up"></i></button>
+                    <span class="cs_quantity_input">{{ item.quantity }}</span>
+                    <button class="cs_quantity_btn cs_decrement" @click="decrementQuantity(item.product_id,item.quantity)"><i
+                        class="fa-solid fa-angle-down"></i></button>
+                  </div>
+                </td>
+                <td>{{ item.quantity * item.regular_price }}</td>
+                <td class="text-center">
+                  <button class="cs_cart-table-close" @click="removeFromCart(item.product_id)"><i
+                      class="fa-solid fa-xmark"></i></button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="cs_height_30 cs_height_lg_30"></div>
+      </div>
+      <div class="col-xl-4">
+        <div class="cs_shop-side-spacing">
+          <div class="cs_shop-card">
+            <h2 class="cs_fs_21 cs_medium">Código de Cupom</h2>
+            <form action="#" class="cs_coupon-doce-form">
+              <input type="text" placeholder="Código do Cupom" style="width: 300px;">
+              <button class="cs_product_btn cs_color1 cs_semi_bold">Aplicar</button>
+            </form>
+            <div class="cs_height_30 cs_height_lg_30"></div>
+            <h2 class="cs_fs_21 cs_medium">Total do Carrinho</h2>
+            <table class="cs_medium">
               <tbody>
-                <tr v-for="item in items" :key="item.product_id">
-                  <td>
-                    <div class="cs_cart_table_media">
-                      <img :src="item.product_image" alt="Thumb">
-                      <h3>{{ item.product_name }}</h3>
-                    </div>
-                  </td>
-                  <td>{{ item.price }}</td>
-                  <td>
-                    <div class="cs_quantity">
-                      <button class="cs_quantity_btn cs_increment"><i class="fa-solid fa-angle-up"></i></button>
-                      <span class="cs_quantity_input">{{ item.quantity }}</span>
-                      <button class="cs_quantity_btn cs_decrement"><i class="fa-solid fa-angle-down"></i></button>
-                    </div>
-                  </td>
-                  <td>{{ item.quantity * item.regular_price}}</td>
-                  <td class="text-center">
-                    <button class="cs_cart-table-close" @click="removeFromCart(item.product_id)"><i class="fa-solid fa-xmark"></i></button>
-                  </td>
+                <tr>
+                  <td>Subtotal</td>
+                  <td class="text-end">{{ totalCart }}</td>
+                </tr>
+                <tr>
+                  <td>Total</td>
+                  <td class="text-end">{{ totalCart }}</td>
                 </tr>
               </tbody>
             </table>
+            <div class="cs_height_30 cs_height_lg_30"></div>
+            <RouterLink :to="{ name: 'checkout' }" class="cs_btn cs_style_1 cs_fs_16 cs_medium w-100">Prosseguir para o
+              Checkout</RouterLink>
           </div>
           <div class="cs_height_30 cs_height_lg_30"></div>
         </div>
-        <div class="col-xl-4">
-          <div class="cs_shop-side-spacing">
-            <div class="cs_shop-card">
-              <h2 class="cs_fs_21 cs_medium">Código de Cupom</h2>
-              <form action="#" class="cs_coupon-doce-form">
-                <input type="text" placeholder="Código do Cupom">
-                <button class="cs_product_btn cs_color1 cs_semi_bold">Aplicar</button>
-              </form>
-              <div class="cs_height_30 cs_height_lg_30"></div>
-              <h2 class="cs_fs_21 cs_medium">Total do Carrinho</h2>
-              <table class="cs_medium">
-                <tbody>
-                  <tr>
-                    <td>Subtotal</td>
-                    <td class="text-end">$605.00</td>
-                  </tr>
-                  <tr>
-                    <td>Total</td>
-                    <td class="text-end">$605.00</td>
-                  </tr>
-                </tbody>
-              </table>
-              <div class="cs_height_30 cs_height_lg_30"></div>
-              <RouterLink :to="{name:'checkout'}" class="cs_btn cs_style_1 cs_fs_16 cs_medium w-100">Prosseguir para o Checkout</RouterLink>
-            </div>
-            <div class="cs_height_30 cs_height_lg_30"></div>
-          </div>
-        </div>
       </div>
     </div>
-    <div class="cs_height_110 cs_height_lg_50"></div>
-    <hr>
-  </template>
+  </div>
+  <div class="cs_height_110 cs_height_lg_50"></div>
+<hr></template>
   
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,41 +1,56 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia';
 
-export const useShoppingCartStore = defineStore('shoppingCart', {
-    state: () => ({
-        cartItems: JSON.parse(localStorage.getItem('cartItems')) || []
-    }),
+export const useShoppingCartStore = defineStore('shoppingCart',{
+  state: () => ({
+    cartItems: JSON.parse(localStorage.getItem('cartItems')) || [],
+  }),
 
-    actions: {
-        addToCart(product_id, product_name, product_image, regular_price, quantity) {
-            const existingProductIndex = this.cartItems.findIndex(item => item.product_id === product_id);
-            if (existingProductIndex !== -1) {
-                this.cartItems[existingProductIndex].quantity += quantity;
-            } else {
-                this.cartItems.push({
-                    product_id,
-                    product_name,
-                    product_image,
-                    quantity,
-                    regular_price
-                });
-            }
+  actions: {
+    addToCart(product_id, product_name, product_image, regular_price, quantity) {
+      const existingProductIndex = this.cartItems.findIndex(item => item.product_id === product_id);
+      if (existingProductIndex !== -1) {
+        this.cartItems[existingProductIndex].quantity += quantity;
+      } else {
+        this.cartItems.push({
+          product_id,
+          product_name,
+          product_image,
+          quantity,
+          regular_price,
+        });
+      }
 
-            localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
+      localStorage.setItem('cartItems', JSON.stringify(this.cartItems));
 
-            console.log("Produto adicionado ao carrinho com sucesso")
-        },
-
-        removeFromCart(product_id) {
-            const updatedCartItems = this.cartItems.filter(item => item.product_id !== product_id);
-            this.cartItems = updatedCartItems;
-
-            localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
-        },
+      console.log('Produto adicionado ao carrinho com sucesso');
     },
 
-    getters: {
-        getCartItems() {
-            return this.cartItems
+    removeFromCart(product_id) {
+      const updatedCartItems = this.cartItems.filter(item => item.product_id !== product_id);
+      this.cartItems = updatedCartItems;
+
+      localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+    },
+
+    updateQuantity(product_id, newQuantity) {
+      const updatedCartItems = this.cartItems.map(item => {
+        if (item.product_id === product_id) {
+          item.quantity = newQuantity;
         }
+        return item;
+      });
+
+      this.cartItems = updatedCartItems;
+      localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
     },
+  },
+
+  getters: {
+    getCartItems() {
+      return this.cartItems;
+    },
+    getTotalCart(){
+        return this.cartItems.reduce((total, item) => total + item.quantity * item.regular_price, 0)
+    }
+  },
 });
